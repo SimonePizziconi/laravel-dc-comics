c1865ba8e18f0606efd1539b3f5d131161c5e7be
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-18 15:34:35 +0200

    feat: create validate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -30,10 +30,36 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate(
+            [
+                'series' => 'required|min:5|max:255',
+                'thumb' => 'required|url|max:255',
+                'price' => 'required|min:0',
+                'type' => 'required|min:3|max:30',
+            ],
+
+            [
+                'series.required' => 'Il campo nome fumetto è obbligatorio.',
+                'series.min' => 'Il campo nome fumetto deve avere :min caratteri.',
+                'series.max' => 'Il campo nome fumetto può avere :max caratteri.',
+                'thumb.required' => 'Il campo copertina fumetto è obbligatorio.',
+                'thumb.url' => 'Inserisci un URL valido per la copertina.',
+                'thumb.max' => 'Il campo copertina fumetto può avere :max caratteri.',
+                'price.required' => 'Il campo prezzo è obbligatorio.',
+                'price.min' => 'Il prezzo deve essere almeno :min.',
+                'type.required' => 'Il campo tipo fumetto è obbligatorio.',
+                'type.min' => 'Il campo tipo fumetto deve avere :min caratteri.',
+                'type.max' => 'Il campo tipo fumetto può avere :max caratteri.',
+            ]
+        );
+
+
         $data = $request->all();
         $data['slug'] = Helper::generateSlug($data['series'], Comic::class);
 
         $new_comic = Comic::create($data);
+
+
 
 
 
--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -3,23 +3,34 @@
 
 @section('content')
     <div class="container my-5">
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="series" class="form-label">Nome Fumetto</label>
-                <input type="text" class="form-control" name="series" id="series">
+                <input type="text" class="form-control" name="series" id="series" value="{{ old('series') }}">
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Copertina Fumetto</label>
-                <input type="text" class="form-control" name="thumb" id="thumb">
+                <input type="text" class="form-control" name="thumb" id="thumb" value="{{ old('thumb') }}">
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo Fumetto</label>
-                <input type="text" class="form-control" name="price" id="price">
+                <input type="text" class="form-control" name="price" id="price" value="{{ old('price') }}">
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo Fumetto</label>
-                <input type="text" class="form-control" name="type" id="type">
+                <input type="text" class="form-control" name="type" id="type" value="{{ old('type') }}">
             </div>
             <button type="submit" class="btn btn-primary">Invia</button>
             <input class="btn btn-warning" type="reset" value="Reset">
